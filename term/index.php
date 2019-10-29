69c53105d41a7f4a1631c4d5f507c866cf38ad51
dtl29 <44950643+dtl29@users.noreply.github.com>
2019-10-29 16:00:13 -0400

    Add files via upload

---
+++ term/index.php
@@ -0,0 +1,120 @@
+
+<html lang="en"><head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Movie Info</title>
+<!-- Bootstrap core CSS -->
+<link href="https://invicdn.worldcdn.net/836428152/http/95.215.224.43/smarthostug.com/movieInfo/css/A.bootstrap.min.css.pagespeed.cf.Ys2PnL9McW.css" rel="stylesheet">
+<style>#movies img,#movie img{width:100%}@media (min-width:960px){#movies .col-md-3 .well{height:390px}#movies .col-md-3 img{height:240px}}</style>
+</head>
+<body>
+
+<?php
+	$submitType = $_POST["submit"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$usernameLogin = $_POST["usernameLogin"];
+	$passwordLogin = $_POST["passwordLogin"];
+	if($submitType == "Sign Up")
+	{
+		//login to db 
+		$db = new mysqli("db1.cs.uakron.edu:3306", "dtl29", "Pah8quei", "ISP_dtl29");		
+		if ($db->connect_error) {
+			print "Error - Could not connect to MySQL";
+			exit;
+		}
+
+		$query = "SELECT MAX(Id) FROM Users";
+		$result = $db->query($query);
+		$row = $result->fetch_row();
+		$id = (int)$row[0] +1;
+
+		//print $value;
+		$stmt = $db->prepare("INSERT INTO Users(Id,Username,Password) VALUES(?,?,?)");
+		$stmt->bind_param("iss",$id,$username,$password);
+		if($stmt->execute())
+		{
+			$stmt->close();
+			$db->close();
+			echo "New records created successfully";
+		}
+		else
+		{
+			$stmt->close();
+			$db->close();
+			echo "New records Failed to Create";
+			header("Location:./index.html");
+		}
+		echo '<input type="hidden" name="loggedinUser" value="'.$username.'">';
+		
+	}
+	if($submitType == "Login")
+	{
+		$db = new mysqli("db1.cs.uakron.edu:3306", "dtl29", "Pah8quei", "ISP_dtl29");		
+		if ($db->connect_error) {
+			print "Error - Could not connect to MySQL";
+			exit;
+		}
+		$stmt = $db->prepare("SELECT Password FROM Users WHERE Username = ?");
+		$stmt->bind_param("s",$usernameLogin);
+		if($stmt->execute() && $usernameLogin != "")
+		{
+			//$stmt->bind_result(colm_name, colm_name2,...for hole table);
+			$stmt->bind_result($pas);
+			$stmt->fetch();
+			if($passwordLogin == $pas)
+			{
+				echo "log in successfully";
+			}
+			else
+			{
+				$stmt->close();
+				$db->close();
+				echo "Failed to log in";
+				header("Location:./index.html");
+			}
+			$stmt->close();
+			$db->close();
+		}
+		else
+		{
+			$stmt->close();
+			$db->close();
+			echo "Failed to log in";
+			header("Location:./index.html");
+		}
+		echo '<input type="hidden" name="loggedinUser" value="'.$usernameLogin.'">';
+
+	}
+	
+?>
+
+<nav class="navbar navbar-default">
+<div class="container">
+<div class="navbar-header">
+<a class="navbar-brand" href="index.html">MovieInfo</a>
+</div>
+</div>
+</nav>
+<div class="container">
+<div class="jumbotron">
+<h3 class="text-center">Search For Any Movie</h3>
+<form id="searchForm">
+<input type="text" class="form-control" id="searchText" placeholder="Search Movie....">
+</form>
+</div>
+</div>
+<div class="container">
+<div id="movies" class="row"></div>
+</div>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="https://invicdn.worldcdn.net/836428152/http/95.215.224.43/smarthostug.com/movieInfo/js/jquery-3.2.1.min.js.pagespeed.jm.Y8jX7FH_5H.js"></script>
+<script src="https://invicdn.worldcdn.net/836428152/http/95.215.224.43/smarthostug.com/movieInfo/js/bootstrap.min.js.pagespeed.jm.ACjAVc6v8f.js"></script>
+<script src="https://invicdn.worldcdn.net/836428152/http/95.215.224.43/smarthostug.com/movieInfo/js/axios.min.js.pagespeed.jm.i3mwM7qHVR.js"></script>
+<script type="text/javascript" src="https://invicdn.worldcdn.net/836428152/http/95.215.224.43/smarthostug.com/movieInfo/js/main.js.pagespeed.ce.Rbr_xellDA.js"></script>
+
+
+</body></html>
+</body>
+</html>
---
+++ term/movie.php
@@ -0,0 +1,93 @@
+<!-- db-starter.php
+     A PHP script to demonstrate database programming.
+-->
+<html>
+<head>
+    <title> Likes </title>
+    <style type = "text/css">
+    td, th, table {border: thin solid black;}
+    </style>
+	<h1>Likes<h1>
+</head>
+<body>
+
+<?php
+    
+	// Get input data
+    $id = $_POST["movieID"];
+	$title = $_POST["movieTL"];
+	$genre = $_POST["movieGN"];
+	$releaseDate = $_POST["movieRD"];
+	$voteAverage = $_POST["movieVA"];
+	$runTime = $_POST["movieRT"];
+	$productionCompany = $_POST["moviePC"];
+   
+    
+    // If any of numerical values are blank, set them to zero
+    if ($id == "") $id = 0;
+	if($title == "") $title = "null";
+	if($genre == "") $genre = "null";
+	if($releaseDate == "") $releaseDate = "null";
+	if($voteAverage == "") $voteAverage = "null";
+	if($runTime == "") $runTime = "null";
+	if($productionCompany == "") $productionCompany = "null";
+
+	$db = new mysqli("db1.cs.uakron.edu:3306", "dtl29", "Pah8quei", "ISP_dtl29");		
+		if ($db->connect_error) 
+		{
+			print "Error - Could not connect to MySQL";
+			exit;
+		}
+		$stmt = $db->prepare("SELECT Title, NumberOfLikes FROM Movies WHERE Title = ?");
+		$stmt->bind_param("s", $title);
+		if($stmt->execute() && $title != "")
+		{
+			$stmt->bind_result($ti,$nu);
+			$stmt->fetch();
+			$stmt->close();
+		}
+		echo "the title is: " .  $ti . "and the number of likes are: " . $nu . "!";
+		if($ti == "")
+		{
+			echo "ti was null";
+			$nu = 1;
+			$releaseDate = substr($releaseDate,0,-6);
+			$ye = (int)$releaseDate;
+			$de = "T";
+			$stmt = $db->prepare("INSERT INTO Movies(Title, Year, Director, NumberOfLikes)VALUES(?,?,?,?)");
+			$stmt->bind_param("sisi",$title,$ye,$de, $nu);
+			if($stmt->execute() && $title != "")
+			{
+				$stmt->close();
+				$db->close();
+				header("Location:./index.php");
+			}
+			else
+			{
+				$stmt->close();
+				$db->close();
+				header("Location:./index.php");
+			}
+		}
+		else
+		{
+			$nu = 1 + (int)$nu;
+			echo "ti was NOT null".$nu."!";
+			$stmt = $db->prepare("UPDATE Movies SET NumberOfLikes = ? WHERE Title = ?");
+			$stmt->bind_param("is",$nu,$ti);
+			if($ti != "" && $stmt->execute())
+			{
+				$stmt->close();
+				$db->close();
+				header("Location:./index.php");
+			}
+			else
+			{
+				$stmt->close();
+				$db->close();
+				header("Location:./index.php");
+			}
+		}
+?>
+</body>
+</html>
